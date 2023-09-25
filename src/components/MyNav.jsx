e2a9c09dab6c99568591969c7592cb31e9d5b5cc
MD SAMIT INTESAR <saadsamit64@gmail.com>
2023-09-25 12:15:55 +0600

    nav complete with router pages

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 const MyNav = () => {
   const [state,setState] = useState(false)
   return (
-    <div className="absolute top-0 left-0 right-0">
+    <div className="absolute top-0 left-0 right-0 z-10">
         <nav>
       <div className="flex flex-row justify-between container mx-auto py-4">
       <Link to="/">
@@ -13,17 +13,17 @@ const MyNav = () => {
       </Link>
       <ul className="hidden sm:flex justify-center items-center">
         <li className="px-4">
-          <NavLink to="/" className="text-base font-bold">
+          <NavLink to="/" className="text-base font-medium">
             Home
           </NavLink>
         </li>
         <li className="px-4">
-          <NavLink to="/donation" className="text-base font-bold">
+          <NavLink to="/donation" className="text-base font-medium">
             Donation
           </NavLink>
         </li>
         <li className="px-4">
-          <NavLink to="/statistics" className="text-base font-bold">
+          <NavLink to="/statistics" className="text-base font-medium">
             Statistics
           </NavLink>
         </li>
@@ -32,17 +32,17 @@ const MyNav = () => {
         <button className="text-3xl" onClick={()=> setState(!state)}>{state? <AiOutlineClose></AiOutlineClose> : <AiOutlineMenu></AiOutlineMenu>}</button>
         {state ? <ul className="flex flex-col items-center shadow-lg rounded-md bg-base-200 py-2 absolute top-14 right-4">
         <li className="px-4 py-1 hover:bg-base-300 rounded-md">
-          <NavLink to="/" className="text-base font-bold">
+          <NavLink to="/" className="text-base font-medium">
             Home
           </NavLink>
         </li>
         <li className="px-4 py-1 hover:bg-base-300 rounded-md">
-          <NavLink to="/donation" className="text-base font-bold">
+          <NavLink to="/donation" className="text-base font-medium">
             Donation
           </NavLink>
         </li>
         <li className="px-4 py-1 hover:bg-base-300 rounded-md">
-          <NavLink to="/statistics" className="text-base font-bold">
+          <NavLink to="/statistics" className="text-base font-medium">
             Statistics
           </NavLink>
         </li>
